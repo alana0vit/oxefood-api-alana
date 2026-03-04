b7872790dbf24b8406105c47e5b7e35ab372d95a
aluno <aluno@gmail.com>
2026-03-04 20:41:30 -0300

    Integrando listagem e update

--- src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
@@ -5,6 +5,8 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EntregadorService {
     @Autowired
@@ -16,5 +18,28 @@ public class EntregadorService {
        entregador.setHabilitado(Boolean.TRUE);
        return repository.save(entregador);
    }
+
+   public List<Entregador> listarTodos() {
+
+        return repository.findAll();
+    }
+
+    public Entregador obterPorID(Long id) {
+
+        return repository.findById(id).get();
+    }
+
+    @Transactional
+    public void update(Long id, Entregador entregadorAlterado) {
+
+        Entregador entregador = repository.findById(id).get();
+        entregador.setNome(entregadorAlterado.getNome());
+        entregador.setDataNascimento(entregadorAlterado.getDataNascimento());
+        entregador.setCpf(entregadorAlterado.getCpf());
+        entregador.setFoneCelular(entregadorAlterado.getFoneCelular());
+        entregador.setFoneFixo(entregadorAlterado.getFoneFixo());
+
+        repository.save(entregador);
+    }
 
 }
--- src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
@@ -5,6 +5,8 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProdutoService {
 @Autowired
@@ -16,4 +18,28 @@ public class ProdutoService {
        produto.setHabilitado(Boolean.TRUE);
        return repository.save(produto);
    }
+
+   public List<Produto> listarTodos() {
+
+        return repository.findAll();
+    }
+
+    public Produto obterPorID(Long id) {
+
+        return repository.findById(id).get();
+    }
+
+    @Transactional
+    public void update(Long id, Produto produtoAlterado) {
+
+        Produto produto = repository.findById(id).get();
+        produto.setTitulo(produtoAlterado.getTitulo());
+        produto.setCodigo(produtoAlterado.getCodigo());
+        produto.setDescricao(produtoAlterado.getDescricao());
+        produto.setValorUnitario(produtoAlterado.getValorUnitario());
+        produto.setTempoEntregaMinimo(produtoAlterado.getTempoEntregaMinimo());
+        produto.setTempoEntregaMaximo(produtoAlterado.getTempoEntregaMaximo());
+
+        repository.save(produto);
+    }
 }
